16f7017e9de885750ce0d6303bd8684991438540
Tonio Serna <antonelo@gmail.com>
2020-12-05 20:26:28 +0100

    Add Minh exercises from the spot.

---
+++ 101-109/thespot_live_interview_alf.rb
@@ -0,0 +1,41 @@
+=begin
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples:
+
+test cases:
+find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]) == 5
+=> 5 appears 3 times in the argument
+
+find_it([1,1,2,-2,5,2,4,4,-1,-2,5]) == -1
+find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]) == 5
+find_it([10]) == 10
+find_it([1,1,1,1,1,1,10,1,1,1,1]) == 10
+
+=end
+
+
+
+
+# parts
+=begin
+- Problem:
+  Given an array of integers, find the one that appears an odd number of times.
+- Rules:
+There will always be only one integer that appears an odd number of times.
+- Input/Output:
+  2
+- Test/examples:
+=end
+find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]) == 5
+find_it([1,1,2,-2,5,2,4,4,-1,-2,5]) == -1
+find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]) == 5
+find_it([10]) == 10
+find_it([1,1,1,1,1,1,10,1,1,1,1]) == 10
+=begin
+- Data structures
+- Algorithm:
+- Code:
+=end
